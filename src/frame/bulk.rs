a961d36a4633cc4513eb28b2f4856de14cf49a49
Apache <apache.software.foundation@gmail.com>
2026-04-09 17:34:47 -0500

    add multithreading to callback_fill

--- src/frame/bulk.rs
+++ src/frame/bulk.rs
@@ -39,16 +39,55 @@ impl Frame {
 		self.data.fill(color);
 	}
 
-	pub fn callback_fill<T: Fn(usize, usize) -> Color>(&mut self, callback: T) {
-		// Use a separate index here to avoid having to recompute it for every pixel
-		// The caveat is that I **MUST** loop row by row for the index to line up
-		let mut i = 0;
-		for y in 0..self.height {
-			for x in 0..self.width {
-				self.set_pixel_i(i, callback(x, y));
-				i += 1;
-			}
+	pub fn callback_fill<T>(&mut self, callback: T)
+	where
+		T: Fn(usize, usize) -> Color + Send + ToOwned<Owned = T>,
+		T: 'static,
+	{
+		const THREAD_COUNT: usize = 8;
+		let width = self.width;
+		let height = self.height;
+		let length = width * height;
+		let mut result: Vec<Color> = Vec::with_capacity(length);
+		let mut thread_handles = Vec::with_capacity(THREAD_COUNT);
+		for thread_id in 0..THREAD_COUNT {
+			let inner_callback = callback.to_owned();
+			let thread = std::thread::spawn(move || {
+				let start_i = (thread_id as f64 * (length / THREAD_COUNT) as f64).ceil() as usize;
+				let mut y = start_i / width;
+				let mut x = start_i - (width * y);
+
+				let end_i =
+					((thread_id + 1) as f64 * (length / THREAD_COUNT) as f64).ceil() as usize;
+
+				let mut chunk = Vec::with_capacity(end_i - start_i + 1);
+
+				for _i in start_i..end_i {
+					chunk.push(inner_callback(x, y));
+					x += 1;
+					if x == width {
+						x = 0;
+						y += 1;
+					}
+				}
+
+				(thread_id, chunk)
+			});
+			thread_handles.push(thread);
 		}
+
+		let mut chunks: Vec<Vec<Color>> = vec![vec![]; THREAD_COUNT];
+
+		for handle in thread_handles {
+			let (thread_id, chunk) = handle.join().unwrap();
+			chunks[thread_id] = chunk;
+		}
+
+		for i in 0..THREAD_COUNT {
+			result.append(&mut chunks[i]);
+		}
+
+		self.data = result;
 	}
 
 	pub fn callback_update<T: Fn(usize, usize, Color) -> Color>(&mut self, callback: T) {
